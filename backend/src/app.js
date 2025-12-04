70cb40185e80c53a27c5a51d477571b8c2203722
Pedro <pfidelis038@gmail.com>
2025-12-04 20:42:36 -0300

    fix(backend): app estruturado

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,15 @@
 import express from "express";
+import cors from "cors";
+import truckRoutes from "./routes/truck_routes.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 
-// middlewares e rotas serão inseridas aqui
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", truckRoutes);
+
+app.use(errorHandler);
 
 export default app;
