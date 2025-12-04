a2ca7d6ebb17957f5ff7be99a0467e38bb84890a
ppodev7 <pfidelis038@gmail.com>
2025-12-03 22:57:34 -0300

    wip(backend): manutencao json e construcao de arquivo app

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { connectDB } from "./config/database";
-import app from "./app";
+import { connectDB } from "./config/database.js";
+import { app } from "./app.js";
 
 const PORT = process.env.PORT || 3001;
 
